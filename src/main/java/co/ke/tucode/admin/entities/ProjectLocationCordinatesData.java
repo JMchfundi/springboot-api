50fd3e16679c999f2d7855a9459050d0d5eb2519
Jafari Mchasa Akida <jakida@tucode.co.ke>
2024-02-02 15:41:04 +0300

    hotel data coding

---
+++ src/main/java/co/ke/tucode/admin/entities/ProjectLocationCordinatesData.java
@@ -0,0 +1,51 @@
+package co.ke.tucode.admin.entities;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectLocationCordinatesData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name="project_location_cordinates_data_generator", 
+    sequenceName = "project_location_cordinates_data_seq", allocationSize=50)    
+    private Integer id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "addr")
+    private String addr;
+
+    @Column(name = "subway_access")
+    private Boolean subway_access;
+
+    @Column(name = "distance_from_center")
+    private Integer distance_from_center;
+
+    @OneToOne(mappedBy = "mainData", fetch = FetchType.LAZY,
+    cascade = CascadeType.ALL)
+    private ProjectLocationCordinatesData locationData;
+
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_main_data_id", nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private ProjectMainData mainData;
+}
